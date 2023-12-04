7af13fd4dd9a222f41467cad72844a19c0210397
melikagolbaz13808080 <golbazmelika@gmail.com>
2023-12-04 13:18:16 +0330

    first commit

---
+++ js/sidenav.js
@@ -0,0 +1,9 @@
+
+
+function openNav() {
+    $("#mySidenav").css('width' , '100%');
+}
+
+function closeNav() {
+    $("#mySidenav").css('width' , '0');
+}
---
+++ js/slider.js
@@ -0,0 +1,7 @@
+// You can also use "$(window).load(function() {"
+$(function () {
+    // Slideshow 3
+    $("#slider3").responsiveSlides({
+        manualControls: '#slider3-pager',
+    });
+});
