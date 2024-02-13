839e66579221d3194b95acd5416426b9b018eca5
melika golbaz <golbazmelika@gmail.com>
2024-02-13 17:05:20 +0330

    [FEATURE, UPDATE] add profile page and fix router and checker login and add can edit profile

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,22 @@
 $(function () {
     $("#includedFooter").load("footer.html");
 });
+
+function isLoggedIn() {
+    if (localStorage.getItem("is_login") == "true") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let profile_nav = document.getElementById('profile-nav')
+let login_nav = document.getElementById('auth-nav')
+
+if (!isLoggedIn()) {
+    profile_nav.style.display = 'none';
+    login_nav.style.display = '';
+} else {
+    profile_nav.style.display = '';
+    login_nav.style.display = 'none';
+}
